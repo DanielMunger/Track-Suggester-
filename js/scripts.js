f548f2b567a6d4b7f600ebfa9205065c11edb30b
Daniel  Munger <danielmunger@Daniels-MacBook-Pro-2.local>
2016-10-21 13:33:16 -0700

    add more JS logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,26 @@
 // Back-End JS
 
-var answer1, answer2, answer3, answer4, answer5, ruby, css, csharp, javandroid, other
+var answer1, answer2, answer3, answer4, answer5, ruby, css, csharp, javandroid, option, other
 
 var calculate = function(answer1, answer2, answer3, answer4) {
 
-  if(
-    answer1 === "app" && answer2 === "creative" && answer4 === "ux" || answer3 === "coffee-shop" && answer1 === "app" && answer5 === "both" || answer1 === "app" && answer3 === "fun-enviornment" && answer4 === "ux" )
+  if(!answer1 || !answer2 || !answer4 || !answer5 )
+  {
+    alert("please make selections.")
+  }
+  else if(answer1 === "app" && answer2 === "creative" && answer3 === "coffee-shop" || answer2 === "creative" && answer3 === "coffee-shop" && answer4 === "ux" || answer1 === "app" && answer2 === "creative" && answer3 === "fun" || answer2 === "creative" && answer3 === "fun" && answer4 === "ux" )
   {
     css();
   }
-  else if(answer1 === "company" && answer2 === "driven" && answer4 === "logic" || answer1 === "app" && answer3 === "fun-enviornment" && answer4 == "logic")
+  else if(answer1 === "company" && answer2 === "driven" && answer3 === "fun-enviornment" || answer2 === "driven" && answer3 === "fun-enviornment" && answer4 === "logic" || answer2 === "driven" && answer3 === "fun-enviornment" && answer4 === "ux")
   {
     ruby();
   }
-  else if (answer1 === "software" && answer3 === "established-business" && answer4 === "logic" || answer2 === "problem" && answer5 === "windows")
+  else if (answer1 === "software" && answer2 === "problem-solving" && answer3 === "established-business" || answer2 === "problem-solving" && answer3 === "established-business" && answer4 === "logic" || answer1 === "software" && answer4 === "logic" && answer5 === "windows")
   {
     csharp();
   }
-  else if (answer1 === "software" && answer3 === "established-business" && answer5 === "both" || answer1 === "software" && answer2 === "creative" || answer2 === "creative" && answer4 === "logic")
+  else if (answer1 === "software" && answer2 === "creative" && answer3 === "established-business" || answer2 === "creative" && answer3 === "established-business" && answer4 === "logic" || answer1 === "software" && answer2 === "creative" && answer3 === "fun" || answer2 === "creative" && answer3 === "fun" && answer4 === "logic")
   {
     javandroid();
   }
@@ -33,30 +36,35 @@ $(document).ready(function() {
 
   ruby = function() {
     $("#ruby").show();
-    $("#css, #c-sharp, #java-android").hide();
+    $("#css, #c-sharp, #java-android, #other").hide();
   }
   css = function() {
     $("#css").show();
-    $("#ruby, #c-sharp, #java-android").hide();
+    $("#ruby, #c-sharp, #java-android, #other").hide();
   }
   csharp = function() {
     $("#c-sharp").show();
-    $("#css, #ruby, #java-android").hide();
+    $("#css, #ruby, #java-android, #other").hide();
   }
   javandroid = function() {
     $("#java-android").show();
-    $("#css, #ruby, #c-sharp").hide();
+    $("#css, #ruby, #c-sharp, #other").hide();
+  }
+  option = function() {
+    $("#java-android, #c-sharp ").show();
+    $("#css, #ruby, #other").hide();
   }
   other = function() {
     $("#other").show();
     $("#css, #ruby, #c-sharp, #java-android").hide();
   }
-  $("#submit").submit(function() {
+  $("#submit").click(function() {
     answer1 = $("#q1").val();
     answer2 = $("#q2").val();
     answer3 = $("#q3").val();
     answer4 = $("#q4").val();
     answer5 = $("#q5").val();
+    calculate(answer1, answer2, answer3, answer4, answer5);
   });
 
 });
